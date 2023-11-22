f910d27cca4095951a0b3da6ce65d9d897360b81
Codrut Rosu <rosucristian21@stud.ase.ro>
2023-11-22 17:04:28 +0200

    Adding the classes.

--- MyProject/Source.cpp
+++ MyProject/Source.cpp
@@ -1,5 +1,28 @@
-# include <iostream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Location {
+private:
+	string name;
+	int maxSeats;
+	int numRows;
+	int numZones;
+	int* seatsPerRow;
+};
+
+class Event {
+private:
+	string name;
+};
+
+class Ticket {
+private:
+	string ticketType;
+	int uniqueID;
+};
 
 int main() {
-	std::cout << std::endl << "Hello World!";
+	return 0;
 }
