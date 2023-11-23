8d6bcf7eb701cf23ed91f9551f16d084bf975558
Codrut Rosu <rosucristian21@stud.ase.ro>
2023-11-23 16:28:27 +0200

    Updating classes, added Util class.

--- MyProject/Source.cpp
+++ MyProject/Source.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 
 using namespace std;
 
-class Location {
-private:
-	string name;
-	int maxSeats;
-	int numRows;
-	int numZones;
-	int* seatsPerRow;
+class Util {
+
 };
 
 class Event {
 private:
-	string name;
+	char name[100];
+	char date[11];
+	char time[6];
+
+public:
 };
+
+class Location {
+private:
+	char name[100];
+	int maxSeats;
+	int numRows;
+	int numZones;
+	int seatsPerRow[10];
+
+public:
+};
+
+
 
 class Ticket {
 private:
 	string ticketType;
 	int uniqueID;
+public:
 };
 
 int main() {
-	return 0;
+	//Event footballEvent("Football Match", "01.12.2023", "18:00");
 }
